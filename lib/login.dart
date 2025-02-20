25d36cd8dabf805d67a76fa18d26ea515a0cff4e
Abdo <abd111saleh@gmail.com>
2025-02-21 00:58:18 +0300

    add login page

---
+++ lib/login.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Icon(Icons.add_a_photo),
+      ),
+    );
+  }
+}
